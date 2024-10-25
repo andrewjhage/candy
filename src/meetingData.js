61137a21d1c6383c26b8cac4fd202ff526d428cf
Hage, Andrew <ahage@iu.edu>
2024-10-25 12:38:58 -0400

    cleaned up bloat

---
+++ src/meetingData.js
@@ -0,0 +1,24 @@
+/** @format */
+
+const meetings = [
+    {
+        topic: "CIT Monthly Meeting",
+        dateTime: "Sept 19th 2024, 2PM-3PM",
+        location: "Knoy Hall West Lafayette",
+        parking: "Midway Garage",
+    },
+    {
+        topic: "Research In Higher Level Ed",
+        dateTime: "September 24th 2024, 1PM-5PM",
+        location: "Stuart Buildings, West Lafayette",
+        parking: "Street Level",
+    },
+    {
+        topic: "Curriculum Planning",
+        dateTime: "October 19th 2024, 4PM-6PM",
+        location: "IO 240, Indianapolis",
+        parking: "North Street Garage",
+    },
+];
+
+export default meetings;
