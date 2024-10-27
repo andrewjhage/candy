5bf7a9afea452a89e5ccc0b4481f992485ec8c24
Andrew Hage <ahage@iu.edu>
2024-10-27 17:40:44 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+// Imports the React Library, ReactDOM, CSS file, and the App component
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Renders the App component to the root element in the public/index.html file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
