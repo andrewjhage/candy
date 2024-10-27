9b008c2e8e6c40ea44f7b98f0f04b8182b7b947d
Andrew Hage <ahage@iu.edu>
2024-10-27 17:39:18 -0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 /** @format */
-
+// Defines a Header component that takes `props` as an argument
 function Header(props) {
     return (
+        // Returns a header element with the heading text passed as a prop
         <header className="App-header">
             <h1>{props.headingText}</h1>
         </header>
     );
 }
-
+// Exports the Header component to be imported in other files
 export default Header;
