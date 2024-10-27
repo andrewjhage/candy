005409839d861467888f1b6df0dd6ea7a49b7530
Andrew Hage <ahage@iu.edu>
2024-10-27 17:31:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+// Importing the CSS file, the Header component and the CandyContainer component files
 import './App.css';
 import Header from './Header';
 import CandyContainer from './CandyContainer';
 
+// Defines a constant variable that holds the  title text for the header
 function App() {
   const headingTitle = 'Charlies Chocolate Factory';
+  // returns the JSX code for the App component
   return (
     <div className="App">
       <Header headingText = {headingTitle}/>
@@ -11,5 +14,5 @@ function App() {
     </div>
   );
 }
-
+// Exports the App component to be imported in other files
 export default App;
