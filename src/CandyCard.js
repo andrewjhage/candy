179ecc1227b3d4f2e37048f70b13bb52f0d8bdc5
Andrew Hage <ahage@iu.edu>
2024-10-27 17:36:36 -0400

    Update CandyCard.js

--- src/CandyCard.js
+++ src/CandyCard.js
@@ -1,23 +1,26 @@
+// Imports the React Library and useState hook from React and the App.css file
 /** @format */
 import React, {useState} from "react";
 import "./App.css";
-
+// Creates a CandyCard component that takes the candy object as a prop
 const CandyCard = ({candy}) => {
+    //Declares a state variable `showModal` to handle if the modal is open or closed
     const [showModal, setShowModal] = useState(false);
-
+    // Function that opens the modal when "Calories" button is clicked
     const handleOpenModal = () => {
         setShowModal(true);
     };
-
+    // Function that closes the modal when "Calories" button is clicked
     const handleCloseModal = () => {    
         setShowModal(false);
     };
 
     return (
+        // JSX code for rendering the candy card
         <div className="candy-card">
             <img
-                src={candy.image}
-                alt={candy.name}
+                src={candy.image} // Uses the image property from the candy object
+                alt={candy.name} // Uses the name property from the candy object
                 style={{
                     padding: '10px 20px',
                     textAlign: 'center',
@@ -25,6 +28,9 @@ const CandyCard = ({candy}) => {
                     background: 'beige',
                     width: '50px',
                 }}
+                // LINE 35 - Displays the name of the candy object
+                // LINE 37 - Button that opens the modal when clicked 
+                // LINE 38 - Conditional rendering of the modal that renders when true
             />
             <h3>{candy.name}</h3>
 
@@ -43,5 +49,5 @@ const CandyCard = ({candy}) => {
             </div>
     );
 };
-
+// Exports the CandyCard component to be imported in other files
 export default CandyCard;
