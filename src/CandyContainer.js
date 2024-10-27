0bfe2e6e0b8336862c722d92f3987778f75ead46
Andrew Hage <ahage@iu.edu>
2024-10-27 17:38:26 -0400

    Update CandyContainer.js

--- src/CandyContainer.js
+++ src/CandyContainer.js
@@ -1,8 +1,12 @@
+// Imports CandyCard component and candy data
 /**  @format */
 import CandyCard from "./CandyCard";
 import candy from "./candyData";
+/* Creates a CandyContainer component that maps through 
+the candy data and renders a CandyCard component for each candy object */
 const CandyContainer = () => {
     return (
+        // JSX code for rendering the candy container
         <div className="candy_container">
             {candy.map((candy, index) =>(
                 <CandyCard key={index} candy={candy}/>
@@ -10,4 +14,5 @@ const CandyContainer = () => {
         </div>
     );
 };
+// Exports the CandyContainer component to be imported in other files
 export default CandyContainer;
